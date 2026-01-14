f92cca7e5fe2cec6e3bb01c149c68e4c885ef0e8
Dibyo Chakraborty <dibyo@handauncle.com>
2026-01-14 18:56:35 +0530

    Initial commit

---
+++ Jan_2026/SeperateSquares.java
@@ -0,0 +1,10 @@
+public class SeperateSquares {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static double seperateSquares(int[][] squares) {
+        return 0.0;
+    }
+}
